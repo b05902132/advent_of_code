797c155205d66ecf2368527c7bf91a23c75e6397
b05902132 <b05902132@csie.ntu.edu.tw>
2021-10-26 23:08:38 +0800

    rename package

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use aoc20;
+use aoc20_20;
 use std::io::{stdin, Read};
 fn main() {
     let stdin = stdin();
     let mut input = String::new();
     stdin.lock().read_to_string(&mut input).unwrap();
-    let out = aoc20::solve_1(&input);
+    let out = aoc20_20::solve_1(&input);
     println!("{}", out);
 }
